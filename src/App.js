c0fea4427a2247de4cd5eb52bb995e83df72939f
karmina-jc <karmina.jarac@gmail.com>
2020-03-12 22:37:08 -0300

    readme

--- src/App.js
+++ src/App.js
@@ -3,15 +3,36 @@ import "./App.css";
 import "./img.svg";
 //import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 //import Mesonero from "./components/Mesonero.jsx";
-//import Cocina from "./components/Cocina.jsx";
-import LunchMenu from "./components/lunchMenu";
+//import Cocina from "./components/Cocina.jsx";;
 
 function App() {
 
 
 
   return (
-
+    <div>
+      <table>
+        <thead>
+          <tr>
+          <th>Food</th>
+          <th>Price</th>
+          <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>      
+          <tr>
+            <td>asdsafd</td>
+            <td>500</td>
+          </tr>
+          <tr>
+          <td>asdasdsa</td>
+          <td>1000</td>
+          </tr>
+        </tbody>
+  
+      </table>
+    
+    </div>
   )
 }
 export default App;
